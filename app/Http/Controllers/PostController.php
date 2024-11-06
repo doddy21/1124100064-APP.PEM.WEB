4b889083b4f00c9f369ac105a89b98a60d18be94
doddy21 <doddy.ilham21@gmail.com>
2024-11-06 14:08:41 +0800

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * index
+     * 
+     * @return void
+     */
+
+    public function index()
+    {
+        //get post
+        $posts = Post::latest()->paginate(5);
+
+        //render view pada posts
+        return view('posts.index', compact('posts'));
+    }
+}
